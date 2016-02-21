bb70af545def83fd6a00697be52feacd2d3bbb17
Aadil Hayat <hayataadil@gmail.com>
2016-02-21 23:53:27 +0530

    Added graph for basic video processing and input using command line flags

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
+tf.app.flags.DEFINE_string("logdir","logs","Directory for saving the logs of computation")
+tf.app.flags.DEFINE_string("vpath","","Path of the video to be processed")
+tf.app.flags.DEFINE_float("resize_ratio",1.0,"Ratio by which each frame should be resized to reduce memory usage")
+tf.app.flags.DEFINE_integer("output_frames",100,"Number of frames in the synopsis video")
+
+FLAGS = tf.app.flags.FLAGS
+
 def get_frames(vpath,resize_ratio):
+    vpath = FLAGS.vpath
+
     cap = cv2.VideoCapture(vpath)
 
     if not cap.isOpened():
@@ -21,18 +30,42 @@ def get_frames(vpath,resize_ratio):
 
     frames = np.zeros((length,height*width, 3), dtype=np.uint8)
 
-    for i in range(0,length+1):
+    for i in range(0,length):
+        ret,frame = cap.read()
         try:
-            ret,frame = cap.read()
             # print ret,frame
-            rframe = cv2.resize(frame,None,fx=resize_ratio,fy=resize_ratio, interpolation=cv2.INTER_CUBIC)
-            # print rframe.shape
-            frames[i] = np.resize(rframe,(height*width,3))
+            if not ret:
+                rframe = cv2.resize(frame,None,fx=resize_ratio,fy=resize_ratio, interpolation=cv2.INTER_CUBIC)
+                # print rframe.shape
+                frames[i] = np.resize(rframe,(height*width,3))
         except Exception as e:
             print "Cannot read video file", e
+            # print i,ret
             break
 
     return frames
 
-fs = get_frames("data/input_video_sample1.mov",0.25)
-print fs.shape,fs
+def video_synopsis(vpath,resize_ratio,output_frames):
+
+    graph = tf.Graph()
+
+    with graph.as_default():
+
+        with tf.name_scope("input") as scope:
+            # video_path = tf.constant(vpath)
+            video_path = tf.constant(0,name="video_path") #commented cause tf.string is not supported yet
+            rRatio = tf.constant(resize_ratio, dtype=tf.float32,name="resize_ratio")
+            k = tf.constant(output_frames, dtype=tf.uint8,name="output_frames")
+
+        with tf.name_scope("video_processing") as scope:
+            frames = tf.py_func(get_frames,[video_path,rRatio],[tf.uint8],name="get_frames")[0]
+
+        sess = tf.Session()
+        writer = tf.train.SummaryWriter(FLAGS.logdir,sess.graph_def)
+
+        fs = sess.run(frames)
+
+        return fs
+
+fs = video_synopsis(FLAGS.vpath,FLAGS.resize_ratio,FLAGS.output_frames)
+print fs.shape
